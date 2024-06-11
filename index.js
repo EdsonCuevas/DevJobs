2a128d8f4ada7ea2e73f3ba946837c96dd9dc9fd
Edson <efelix.cuevas@gmail.com>
2024-06-11 00:38:06 -0600

    add bdmongo config and connection

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+const mongoose = require('mongoose')
+require('./config/db.js')
+
 const express = require('express')
 const exphbs = require('express-handlebars');
 const path = require('path')
 const router = require('./routes/index.js')
 require('dotenv').config({ path: 'variables.env' })
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const MongoStore = require('connect-mongo');
 
 const app = express()
 
@@ -16,6 +22,18 @@ app.set('view engine', 'handlebars')
 
 // Static files
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(cookieParser())
+app.use(session({
+    secret: process.env.SECRETO,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.DATABASE,
+        mongooseConnection: mongoose.connection
+    })
+}));
 
 app.use('/', router())
 
