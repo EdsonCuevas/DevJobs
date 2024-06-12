07a915015f0eb144522b7569554a963ac7363603
Edson <efelix.cuevas@gmail.com>
2024-06-12 04:18:12 -0600

    add show vacantes db

--- controllers/vacantesController.js
+++ controllers/vacantesController.js
@@ -14,12 +14,28 @@ exports.agregarVacante = async (req, res) => {
     const vacante = new Vacante(req.body)
 
     // Crear arreglo de skills
-    vacante.skills = req.body.skills.split()
+    vacante.skills = req.body.skills.split(',')
 
     // Almacenarlo en la db
     const nuevaVacante = await vacante.save()
 
     // Redireccionar
     res.redirect(`/vacantes/${nuevaVacante.url}`)
+
+}
+
+// Muestra una vacante
+exports.mostrarVacante = async (req, res, next) => {
+
+    const vacante = await Vacante.findOne({ url: req.params.url }).lean()
+
+    // Si no hay resultados
+    if (!vacante) return next()
+
+    res.render('vacante', {
+        vacante,
+        nombrePagina: vacante.titulo,
+        barra: true
+    })
 
 }
