d7cdf89139a9923a001164f3dbd31a6d2fe535be
Edson <efelix.cuevas@gmail.com>
2024-06-17 07:51:53 -0600

    add validate uploads

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -5,32 +5,43 @@ const shortid = require('shortid')
 
 exports.subirImagen = (req, res, next) => {
     upload(req, res, function (error) {
-        if (error instanceof multer.MulterError) {
-            return next()
+        if (error) {
+            if (error instanceof multer.MulterError) {
+                if (error.code === 'LIMIT_FILE_SIZE') {
+                    req.flash('error', 'El archivo es muy grande: Máximo 100kb ');
+                } else {
+                    req.flash('error', error.message);
+                }
+            } else {
+                req.flash('error', error.message);
+            }
+            res.redirect('/administracion');
+            return;
+        } else {
+            return next();
         }
-    })
-    next()
+    });
 }
-
-// Opciones de multer
+// Opciones de Multer
 const configuracionMulter = {
+    limits: { fileSize: 100000 },
     storage: fileStorage = multer.diskStorage({
         destination: (req, file, cb) => {
-            cb(null, __dirname + '../../public/uploads/perfiles')
+            cb(null, __dirname + '../../public/uploads/perfiles');
         },
         filename: (req, file, cb) => {
-            const extension = file.mimetype.split('/')[1]
-            cb(null, `${shortid.generate()}.${extension}`)
+            const extension = file.mimetype.split('/')[1];
+            cb(null, `${shortid.generate()}.${extension}`);
         }
     }),
     fileFilter(req, file, cb) {
         if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-            cb(null, true)
+            // el callback se ejecuta como true o false : true cuando la imagen se acepta
+            cb(null, true);
         } else {
-            cb(null, false)
+            cb(new Error('Formato No Válido'));
         }
-    },
-    limit: { fileSize: 100000 }
+    }
 }
 
 const upload = multer(configuracionMulter).single('imagen')
