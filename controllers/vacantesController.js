fdd2e4f5923e143557c088128c02c905bd5395d2
Edson <efelix.cuevas@gmail.com>
2024-06-20 02:37:17 -0600

    add view and controller candidatos

--- controllers/vacantesController.js
+++ controllers/vacantesController.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose")
 
 const Vacante = mongoose.model('Vacante')
 
+const multer = require('multer')
+const shortid = require('shortid')
+const { cerrarSesion } = require("./authController")
+
 exports.formularioNuevaVacante = (req, res) => {
     res.render('nueva-vacante', {
         nombrePagina: 'Nueva Vacante',
@@ -132,5 +136,93 @@ const verificarAutor = (vacante = {}, usuario = {}) => {
         return false
     }
     return true
+
+}
+// Subir archivos en PDF
+exports.subirCV = (req, res, next) => {
+    upload(req, res, function (error) {
+        if (error) {
+            if (error instanceof multer.MulterError) {
+                if (error.code === 'LIMIT_FILE_SIZE') {
+                    req.flash('error', 'El archivo es muy grande: Máximo 100kb ');
+                } else {
+                    req.flash('error', error.message);
+                }
+            } else {
+                req.flash('error', error.message);
+            }
+            res.redirect('back');
+            return;
+        } else {
+            return next();
+        }
+    });
+}
+
+// Opciones de Multer
+const configuracionMulter = {
+    limits: { fileSize: 100000 },
+    storage: fileStorage = multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, __dirname + '../../public/uploads/cv');
+        },
+        filename: (req, file, cb) => {
+            const extension = file.mimetype.split('/')[1];
+            cb(null, `${shortid.generate()}.${extension}`);
+        }
+    }),
+    fileFilter(req, file, cb) {
+        if (file.mimetype === 'application/pdf') {
+            // el callback se ejecuta como true o false : true cuando la imagen se acepta
+            cb(null, true);
+        } else {
+            cb(new Error('Formato No Válido'));
+        }
+    }
+}
+
+const upload = multer(configuracionMulter).single('cv')
+
+//Almacenar los candidatos en la db
+exports.contactar = async (req, res, next) => {
+    //busqueda en la bd con la url de la vacante
+    const vacante = await Vacante.findOne({ url: req.params.url })
+
+    // si no existe la vacante
+    if (!vacante) return next()
+
+    // todo bien, se construye el nuevo obj
+    const nuevoCandidato = {
+        nombre: req.body.nombre,
+        email: req.body.email,
+        cv: req.file.filename
+    }
+
+    // Almacenarla
+    vacante.candidatos.push(nuevoCandidato)
+    await vacante.save()
+
+    // Mensaje flash y redireccion
+    req.flash('correcto', 'Se Envio tu CV Correctamente')
+    res.redirect('/')
+
+}
+
+exports.mostrarCandidatos = async (req, res, next) => {
+    const vacante = await Vacante.findById(req.params.id).lean()
+
+    if (vacante.autor != req.user._id.toString()) {
+        return next()
+    }
+
+    if (!vacante) return next()
+
+    res.render('candidatos', {
+        nombrePagina: `Candidatos Vacante - ${vacante.titulo}`,
+        cerrarSesion: true,
+        nombre: req.user.nombre,
+        imagen: req.user.imagen,
+        candidatos: vacante.candidatos
+    })
 
 }
--- routes/index.js
+++ routes/index.js
@@ -68,5 +68,17 @@ module.exports = () => {
         usuariosController.subirImagen,
         usuariosController.editarPerfil)
 
+    // Recibir mensajes de los candidatos
+    router.post('/vacantes/:url',
+        vacantesController.subirCV,
+        vacantesController.contactar
+    )
+
+    // Muestra los candidatos por vacante
+    router.get('/candidatos/:id',
+        authController.verificarUsuario,
+        vacantesController.mostrarCandidatos
+    )
+
     return router
 }
