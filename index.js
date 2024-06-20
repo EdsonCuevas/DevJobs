c34f8bda31ee7af4c21d94d96f23457bf92820e1
Edson <efelix.cuevas@gmail.com>
2024-06-20 12:13:46 -0600

    add error managment

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const MongoStore = require('connect-mongo');
 const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
 const flash = require('connect-flash')
+const createError = require('http-errors')
 const passport = require('./config/passport.js')
 
 const app = express()
@@ -61,7 +62,21 @@ app.use((req, res, next) => {
 })
 
 
-
 app.use('/', router())
+
+// 404 Pagina no existente
+app.use((req, res, next) => {
+    next(createError(404, 'No Encontrado'))
+})
+
+// Administracion de los errores
+app.use((error, req, res) => {
+    res.locals.mensaje = error.message
+    const status = error.status || 500
+    res.locals.status = status
+    res.status(status)
+
+    res.render('error')
+})
 
 app.listen(process.env.PUERTO)
