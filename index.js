9036bea3771b67356c9beba2c69b6fbfba8d768f
Edson <efelix.cuevas@gmail.com>
2024-06-21 02:48:27 -0600

    upload to deployment heroku

--- index.js
+++ index.js
@@ -79,4 +79,10 @@ app.use((error, req, res) => {
     res.render('error')
 })
 
-app.listen(process.env.PUERTO)
+// Dejar que heroku asigne el puerto
+const host = '0.0.0.0'
+const port = process.env.PORT
+
+app.listen(port, host, () => {
+    console.log('El server esta corriendo')
+})
